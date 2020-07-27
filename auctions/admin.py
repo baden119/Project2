e64d5a8ffe8ab2c4450406d8ee746af58ff4bf69
baden allen <baden.j.allen@gmail.com>
2020-07-27 17:50:45 +1000

    Models created and registered (Listing, Comment, Bid)

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Listing, Comment, Bid
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ( "__str__", "description", "starting_bid")
+
+class CommentAdmin(admin.ModelAdmin):
+    comment_display = ("__str__")
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Comment)
+admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,62 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    #  category syntax taken from https://docs.djangoproject.com/en/3.0/ref/models/fields/
+    FASHION = 'FN'
+    HEALTH = 'HB'
+    TOYS = 'TY'
+    BOOKS = 'BK'
+    FOOD = 'FD'
+    TECHNOLOGY = 'TK'
+    HOME = 'HM'
+    GIFTS = 'GF'
+    AUTO = 'AT'
+    OTHER = 'OT'
+    CATAGORY_CHOICES = [
+        (FASHION, 'Fashion, Clothing & Accessories'),
+        (HEALTH, 'Health & Beauty'),
+        (TOYS, 'Toys & Baby Equipment'),
+        (BOOKS, 'Books, Vinyl and Other Physical Media'),
+        (FOOD, 'Groceries, Food and Drinks'),
+        (TECHNOLOGY, 'Technology including Phones and Laptops'),
+        (HOME, 'Home and Furniture'),
+        (GIFTS, 'Flowers and Gifts'),
+        (AUTO, 'Vehicles and Automotive'),
+        (OTHER, 'Other'),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
+    listed_datetime = models.DateTimeField('date listed')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_URL = models.URLField(max_length=200, blank=True)
+    category = models.CharField(
+        max_length=2,
+        choices=CATAGORY_CHOICES,
+        default = OTHER,
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    comment_datetime = models.DateTimeField('date commented')
+
+    def __str__(self):
+        return f"{self.user} - {self.listing} - ({self.comment_datetime.ctime()})"
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment_datetime = models.DateTimeField('date of bid')
+    bid = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.listing} - {self.user} - ${self.bid}"
